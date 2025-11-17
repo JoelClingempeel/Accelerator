0363efcf33e497e7dd4ff3ae3a52a467040ffb17
Joel Clingempeel <joel.clingempeel@gmail.com>
2025-11-17 17:13:55 -0500

    Created instruction for matrix multiplication.

--- accelerator.v
+++ accelerator.v
@@ -8,6 +8,24 @@ parameter BUFFER_LEN = 32;
 parameter GRID_SIZE = 2;
 parameter ADDRESS_LEN = 3;
 
+// TODO Stop hardcoding.
+reg [31:0] instructions [15:0];
+reg [7:0] memory [31:0];
+reg [4:0] pc;
+// Slice up current instruction into opcode and operands.
+wire [31:0] curr_instruction;
+assign curr_instruction = instructions[pc];
+wire [5:0] opcode;
+assign opcode = curr_instruction[23:18];
+wire [3:0] operand1, operand2, operand3;
+assign operand1 = curr_instruction[17:13];
+assign operand2 = curr_instruction[12:8];
+assign operand3 = curr_instruction[7:3];
+wire [2:0] operand4;
+assign operand4 = curr_instruction[2:0];
+
+reg [3:0] mat_mult_stage;
+
 reg ce;
 
 reg [NUM_SIZE-1:0] north_buffer[GRID_SIZE-1:0][BUFFER_LEN-1:0];
@@ -30,6 +48,11 @@ wire [15:0] result[1:0][1:0];
     assign result01 = result[0][1];
     assign result10 = result[1][0];
     assign result11 = result[1][1];
+    wire [15:0] out00, out01, out10, out11;
+    assign out00 = memory[8];
+    assign out01 = memory[9];
+    assign out10 = memory[10];
+    assign out11 = memory[11];
 `endif
 
 genvar i,j;
@@ -53,7 +76,7 @@ mxu my_mxu(
     .result_out(result_out)
 );
 
-integer k, l;
+integer k, l, m;
 always @(posedge clk or posedge rst) begin
     if (rst) begin
         for (k = 0; k < GRID_SIZE; k++) begin
@@ -65,7 +88,72 @@ always @(posedge clk or posedge rst) begin
             end
         end
         ce <= 0;
-    end else begin
+        for (m = 0; m < 32; m++) begin
+            instructions[m] <= 0;
+            memory[m] <= 0;
+        end
+        pc <= 0;
+        mat_mult_stage <= 0;
+    end else begin  // Rising clk edge
+        if (mat_mult_stage > 0) begin
+            case (mat_mult_stage)
+                1: begin
+                    west_buffer[0][0] <= memory[operand1];
+                end
+                2: begin
+                    west_buffer[0][1] <= memory[operand1+1];
+                end
+                3: begin
+                    west_buffer[1][1] <= memory[operand1+2];
+                end
+                4: begin
+                    west_buffer[1][2] <= memory[operand1+3];
+                end
+                5: begin
+                    north_buffer[0][0] <= memory[operand2];
+                end
+                6: begin
+                    north_buffer[0][1] <= memory[operand2+2];
+                end
+                7: begin
+                    north_buffer[1][1] <= memory[operand2+1];
+                end
+                8: begin
+                    north_buffer[1][2] <= memory[operand2+3];
+                    ce <= 1;  // Start systolic array.
+                end
+                9: begin
+                end
+                10: begin
+                end
+                11: begin
+                    memory[operand3] <= result00;
+                end
+                12: begin
+                    memory[operand3+1] <= result01;
+                end
+                13: begin
+                    memory[operand3+2] <= result10;
+                end
+                14: begin
+                    memory[operand3+3] <= result11;
+                    ce <= 0;  // Stop systolic array.
+                    pc <= pc + 1;
+                end
+                default: begin
+                end
+            endcase
+            if (mat_mult_stage == 18) begin
+                mat_mult_stage <= 0;
+            end else begin
+                mat_mult_stage <= mat_mult_stage + 1;
+            end
+        end else if (opcode == 8'd1) begin  // Mat Mult
+            mat_mult_stage <= 1;
+        end else begin  // No-op
+            pc <= pc + 1;
+        end
+
         if (ce) begin
             // TODO Add wrap around for out of bounds.
             for (k = 0; k < GRID_SIZE; k++) begin
--- accelerator_tb.v
+++ accelerator_tb.v
@@ -5,7 +5,7 @@ module accelerator_tb();
 reg clk = 0;
 reg rst = 0;
 
-accelerator #(.NUM_SIZE(16), .BUFFER_LEN(8), .GRID_SIZE(2)) uut (
+accelerator #(.NUM_SIZE(16), .BUFFER_LEN(32), .GRID_SIZE(2)) uut (
     .clk(clk),
     .rst(rst)
 );
@@ -20,14 +20,19 @@ initial begin
     rst = ~rst;
     # 10
     rst = ~rst;
-    uut.north_buffer[0][0] = 16'd2;
-    uut.north_buffer[0][1] = 16'd7;
-    uut.north_buffer[1][1] = 16'd1;
-    uut.north_buffer[1][2] = 16'd8;
-    uut.west_buffer[0][0] = 16'd3;
-    uut.west_buffer[0][1] = 16'd1;
-    uut.west_buffer[1][1] = 16'd4;
-    uut.west_buffer[1][2] = 16'd1;
+    uut.instructions[4][23:18] = 6'd1;
+    uut.instructions[4][17:13] = 5'd0;
+    uut.instructions[4][12:8] = 5'd4;
+    uut.instructions[4][7:3] = 5'd8;
+    uut.instructions[4][2:0] = 3'd0;
+    uut.memory[0] = 16'd3;
+    uut.memory[1] = 16'd1;
+    uut.memory[2] = 16'd4;
+    uut.memory[3] = 16'd1;
+    uut.memory[4] = 16'd2;
+    uut.memory[5] = 16'd1;
+    uut.memory[6] = 16'd7;
+    uut.memory[7] = 16'd8;
     # 300
     uut.ce = 1;
     # 500
